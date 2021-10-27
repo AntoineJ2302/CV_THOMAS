bf4ae538e0c4b9ad02e8b9238f726b568499d4cb
AntoineJ2302 <90253800+AntoineJ2302@users.noreply.github.com>
2021-10-27 10:39:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function flip() {
+    let el = document.querySelector('.card-one')
+    el.classList.toggle('flip')
+}
+function fliptwo() {
+    let el = document.querySelector('.card-two')
+    el.classList.toggle('flip')
+}
+function flipthree() {
+    let el = document.querySelector('.card-three')
+    el.classList.toggle('flip')
+}
+function flipfour() {
+    let el = document.querySelector('.card-four')
+    el.classList.toggle('flip')
+}
+
